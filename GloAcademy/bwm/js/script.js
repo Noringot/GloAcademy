cebf20c5a2b17ff746356486ef3553a4a6053bc8
Noringot <73204180+Noringot@users.noreply.github.com>
2021-05-19 13:26:51 +0300

    second commit#2

--- GloAcademy/bwm/js/script.js
+++ GloAcademy/bwm/js/script.js
@@ -1,38 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
+	//Tabs
 	const techCharBtns = document.querySelectorAll('.feature__link'),
-			overflow = document.querySelectorAll('.feature__item');
+			overflow = document.querySelectorAll('.feature__item'),
+			interior = document.querySelectorAll('[data-tabs-field="interior"]'),
+			body = document.querySelectorAll('[data-tabs-field="body"]'),
+			designBtn = document.querySelectorAll('button.design-list__item');
+			console.log(interior);
 	
-	overflow.forEach( (item) =>{
-		// item.style.overflow = 'hidden';
-	});
 
 	techCharBtns.forEach( (item) => {
 		item.addEventListener('click', event => {
 			
 			if(!item.classList.contains('feature__link_active')){
-				remove();
-				active(item);
+				techCharBtns.forEach( (item) => {
+					item.classList.remove('feature__link_active');
+					item.parentElement.querySelector('.feature-sub').classList.add('hidden');
+				});
+
+				item.classList.add('feature__link_active');
+				item.parentElement.querySelector('.feature-sub').classList.remove('hidden');
 			}else{
-				remove();
-			}
-			
-			
+				techCharBtns.forEach( (item) => {
+					item.classList.remove('feature__link_active');
+					item.parentElement.querySelector('.feature-sub').classList.add('hidden');
+				});
+			}			
 		});
 	});
 
-	function active(item) {
-		item.classList.add('feature__link_active');
-		item.parentElement.querySelector('.feature-sub').classList.remove('hidden');
-	}
-
-	function remove() {
-		techCharBtns.forEach( (item) => {
-			item.classList.remove('feature__link_active');
-			item.parentElement.querySelector('.feature-sub').classList.add('hidden');
+	
+	designBtn.forEach((item, i)=> {
+		item.addEventListener('click', event => {
+			designBtn.forEach(btn => btn.classList.remove('design-list__item_active'));
+			item.classList.add('design-list__item_active');
+			if(i === 0){
+				body.forEach(el => el.classList.add('hidden'));
+				interior.forEach(el => el.classList.remove('hidden'));
+			}else{
+				body.forEach(el => el.classList.remove('hidden'));
+				interior.forEach(el => el.classList.add('hidden'));
+			}
 		});
-	}
+	})
 
+
+	// Scroll
+	const scrollElements = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+	scrollElements.forEach(item => {
+		item.addEventListener('click', event => {
+			event.preventDefault();
+			
+			const id = item.getAttribute('href').substring(1);
+			document.getElementById(id).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
+	
 
 });
